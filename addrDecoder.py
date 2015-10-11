38e1c0eede0920e272cd7b305ba9579fd6089a35
agallo <agallo@gwu.edu>
2015-10-11 19:03:34 -0400

    fixing spacing in print statement

--- addrDecoder.py
+++ addrDecoder.py
@@ -31,7 +31,7 @@ def sequence(repeat):
     count = 0
     while count <= repeat:
         for pos in range(0, 8):
-            print "Decimal Position (Selected Chip): " + str(pos) + "Binary address " + str(bin(pos))
+            print "Decimal Position (Selected Chip): " + str(pos) + ", Binary address " + str(bin(pos))
             if 1 & pos != 0:
                 wiringpi.digitalWrite(A0, HIGH)
                 print "     A0 Active : " + str(1 & pos)
